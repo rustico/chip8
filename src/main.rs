425eefa78426afe53a2786b9fb0a3287a1a5f8ed
Nico Bases <nmbases@protonmail.com>
2020-07-01 20:27:17 -0300

    space invadres working

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use rand::Rng;
 use std::{thread, time};
 
-use pancurses::{initscr, endwin, Window, noecho, start_color, COLOR_CYAN, COLOR_BLACK, init_pair, COLOR_PAIR, Input};
+use pancurses::{initscr, endwin, Window, noecho, start_color, COLOR_CYAN, COLOR_BLACK, init_pair, Input};
 
 const APP_START: u16 = 0x200;
 const SCREEN_WIDTH: usize = 64;
@@ -14,13 +14,15 @@ const NO_CURSES: bool = false;
 const BREAKPOINT: usize = 0;
 
 fn main() {
+    //let numero = 0x3;
+    //let numero2 = 0xF;
+    //let numero3 = numero <<  4 | numero2;
+    //println!("{:x}", numero3);
+
     let mut chip8 = Chip8::new();
-    chip8.load("pong.ch8").unwrap();
-    //chip8.load("space.ch8").unwrap();
+    //chip8.load("pong.ch8").unwrap();
+    chip8.load("space.ch8").unwrap();
     //chip8.load("zero.ch8").unwrap();
-    if NO_CURSES {
-        endwin();
-    }
     chip8.start();
 }
 
@@ -63,6 +65,10 @@ impl Chip8 {
         start_color();
         init_pair(1, COLOR_CYAN, COLOR_BLACK);
 
+        if  NO_CURSES {
+            endwin();
+        }
+
         Chip8 {
             registers: (0..=15).map(|_| 0).collect(),
             memory: (0..=4096).map(|_| 0).collect(),
@@ -71,16 +77,16 @@ impl Chip8 {
             stack: Vec::with_capacity(16),
             stack_pointer: 0,
             screen: (0..=(SCREEN_WIDTH * SCREEN_HEIGHT)).map(|_| 0).collect(),
-            window: window,
-            display: display,
+            window,
+            display,
             size: 0,
             cycles: 0,
             timer: 0,
             keypad: (0..=15).map(|_| 0).collect(),
             sound_timer: 0,
             logs: Vec::new(),
-            debug_window: debug_window,
-            log_window: log_window,
+            debug_window,
+            log_window,
         }
     }
 
@@ -102,30 +108,33 @@ impl Chip8 {
         loop {
             if !NO_CURSES {
                 self.draw();
+
+                match self.window.getch() {
+                    Some(Input::KeyEnter) => break,
+                    Some(Input::Character('1')) => { self.keypad[0] = 1; } // 1
+                    Some(Input::Character('2')) => { self.keypad[1] = 1; } // 2
+                    Some(Input::Character('3')) => { self.keypad[2] = 1; } // 3
+                    Some(Input::Character('4')) => { self.keypad[4] = 1; } // C
+                    Some(Input::Character('q')) => { self.keypad[5] = 1; } // 4
+                    Some(Input::Character('w')) => { self.keypad[6] = 1; } // 5
+                    Some(Input::Character('e')) => { self.keypad[7] = 1; } // 5
+                    Some(Input::Character('r')) => { self.keypad[8] = 1; } // D
+                    Some(Input::Character('a')) => { self.keypad[9] = 1; } // 7
+                    Some(Input::Character('s')) => { self.keypad[0xA] = 1; } // 8
+                    Some(Input::Character('d')) => { self.keypad[0xB] = 1; } // 9
+                    Some(Input::Character('f')) => { self.keypad[0xC] = 1; } // E
+                    Some(Input::Character('z')) => { self.keypad[0xD] = 1; } // A
+                    Some(Input::Character('x')) => { self.keypad[0xE] = 1; } // 0
+                    Some(Input::Character('c')) => { self.keypad[0xF] = 1; } // B
+                    Some(Input::Character('v')) => { self.keypad[0x10] = 1; } // F
+                    _ => ()
+                }
             }
+
             if BREAKPOINT > 0 && BREAKPOINT == self.cycles {
                 self.window.nodelay(false);
             }
 
-            match self.window.getch() {
-                Some(Input::KeyEnter) => break,
-                Some(Input::Character('1')) => { self.keypad[0] = 1; }
-                Some(Input::Character('w')) => { self.keypad[1] = 1; }
-                Some(Input::Character('3')) => { self.keypad[2] = 1; }
-                Some(Input::Character('s')) => { self.keypad[4] = 1; }
-                Some(Input::Character('4')) => { self.keypad[5] = 1; }
-                Some(Input::Character('5')) => { self.keypad[6] = 1; }
-                Some(Input::Character('6')) => { self.keypad[7] = 1; }
-                Some(Input::Character('7')) => { self.keypad[8] = 1; }
-                Some(Input::Character('8')) => { self.keypad[9] = 1; }
-                Some(Input::Character('9')) => { self.keypad[0xA] = 1; }
-                Some(Input::Character('e')) => { self.keypad[0xB] = 1; }
-                Some(Input::Character('a')) => { self.keypad[0xC] = 1; }
-                Some(Input::Character('0')) => { self.keypad[0xD] = 1; }
-                Some(Input::Character('b')) => { self.keypad[0xE] = 1; }
-                Some(Input::Character('f')) => { self.keypad[0xF] = 1; }
-                _ => ()
-            }
 
             self.cycle();
         }
@@ -263,7 +272,7 @@ impl Chip8 {
                 let vx = self.registers[x as usize];
                 let kk: u8 = (k1 << 4) | k2; 
 
-                self.logs.push(format!("{:04X}: {:X}{n1:X}{n2:X}{n3:X} \t JNE V{n1:X}, {n2:X}{n3:X}", self.pc - 2, 4, n1=x, n2=k1, n3=k2));
+                self.logs.push(format!("{:04X}: {:X}{n1:X}{n2:X}{n3:X} \t JNE V{n1:X}, {kk:X}", self.pc - 2, 4, n1=x, n2=k1, n3=k2, kk=kk));
                 if vx != kk {
                     self.pc += 2;
                 }
@@ -378,18 +387,21 @@ impl Chip8 {
                 let vx = self.registers[x as usize];
                 let vy = self.registers[y as usize];
 
+                self.logs.push(format!("{:04X}: {:X}{n1:X}{n2:X}{n3:X} \t JNE V{n1:X}, V{n2:X}", self.pc - 2, 9, n1=x, n2=y, n3=0));
                 if vx != vy {
                     self.pc += 2;
                 }
             },
             (0xA, n1, n2, n3) => {
                 // Set I = nnn.
+                self.logs.push(format!("{:04X}: {:X}{n1:X}{n2:X}{n3:X} \t MOV I, {n1:X}{n2:X}{n3:X}", self.pc - 2, 0xA, n1=n1, n2=n2, n3=n3));
                 self.index = (n1 as u16) << 8 | (n2 as u16) << 4 | (n3 as u16);
             },
             (0xB, n1, n2, n3) => {
                 //  Jump to location nnn + V0.
                 let v0 = self.registers[0];
                 let addr = (n1 as u16) << 8 | (n2 as u16) << 4 | (n3 as u16);
+                self.logs.push(format!("{:04X}: {:X}{n1:X}{n2:X}{n3:X} \t JMP {addr:X}", self.pc - 2, 0xB, n1=n1, n2=n2, n3=n3, addr=addr));
                 self.pc = v0 as u16 + addr;
             },
             (0xC, x, k1, k2) => {
@@ -397,29 +409,48 @@ impl Chip8 {
                 let kk: u8 = (k1 << 4) | k2; 
                 let mut rng = rand::thread_rng();
                 let value: u8 = rng.gen();
+                self.logs.push(format!("{:04X}: {:X}{x:X}{k1:X}{k2:X} \t MOV V{x:x}, {value:X}", self.pc - 2, 0xC, x=x, k1=k1, k2=k2, value=value));
                 self.registers[x as usize] = value & kk;
             },
-            (0xD, x, y, n) => {
+            (0xD, vx, vy, n) => {
                 // Draw sprite, x, y, number of bytes
-                let x = self.registers[x as usize];
-                let y = self.registers[y as usize];
+                let base_x = self.registers[vx as usize];
+                let base_y = self.registers[vy as usize];
+                self.logs.push(format!("{:04X}: {:X}{vx:X}{vy:X}{n:X} \t DRW {x:X},{y:X}", self.pc - 2, 0xD, x=base_x, y=base_y, n=n, vx=vx,vy=vy));
 
-                self.registers[0xF] = 0;
+                let mut vf_value = 0;
+
                 for row in 0..n {
                     let sprite = self.memory[(self.index + row as u16)as usize];
-                    let y = (y as usize + row as usize)  % SCREEN_HEIGHT;
+                    let y = (base_y as usize + row as usize) % SCREEN_HEIGHT;
 
+                    self.logs.push(format!("{:b}", sprite));
                     for column in 0..8 {
                         let pixel = (sprite >> (7 - column)) & 0b0000_0001;
-                        let x = (x as usize + column) % SCREEN_WIDTH;
+                        //let x = (x as usize + column) % SCREEN_WIDTH;
+                        let mut x = (base_x as usize + column);
+                        if x >= SCREEN_WIDTH {
+                            x = SCREEN_WIDTH - (x as usize % SCREEN_WIDTH);
+                        }
+
                         let coordinates = (y * SCREEN_WIDTH) + x; 
+
+
                         if pixel == 1 && self.screen[coordinates] == 1 {
-                            self.registers[0xF] = 1;
+                            self.logs.push(format!("{:?}", column));
+                            vf_value = 1;
+
+                            if x == 61 && pixel==1 {
+                                //self.window.nodelay(false);
+                            }
                         }
 
                         self.screen[coordinates] ^= pixel;
                     }
                 }
+
+                self.registers[0xF] = vf_value;
+                self.logs.push(format!("{:04X}: {:X}{vx:X}{vy:X}{n:X} \t MOV VF, {value:X}", self.pc - 2, 0xD, n=n, vx=vx,vy=vy, value=vf_value));
             },
             (0xE, x, 9, 0xE) => {
                 // Skip next instruction if key with the value of Vx is pressed.
@@ -480,6 +511,13 @@ impl Chip8 {
                 for i in 0..=x {
                     let value = self.memory[self.index as usize + i as usize];
                     self.registers[i as usize] = value;
+                }
+            },
+            (0x0, 0x0, 0xE, 0) => {
+                // CLEAN SCREEN
+                self.logs.push(format!("{:04X}: 00E0 \t CLS", self.pc - 2));
+                for i in 0..SCREEN_WIDTH * SCREEN_HEIGHT {
+                    self.screen[i] = 0;
                 }
             },
             (a, b, c, d) => {
